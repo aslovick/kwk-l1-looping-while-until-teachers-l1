9c67a5ae469da2ad1fd9f2ab3f0f5be112150c5f
Abigail Slovick <abby.slovick@gmail.com>
2018-06-20 00:04:43 +0000

    Done.

--- spec/while_spec.rb
+++ spec/while_spec.rb
@@ -1,18 +1,8 @@
-require_relative "spec_helper"
-require_relative "../lib/while.rb"
-
-describe "#using_until"
-	do
-		it
-		if force < 10
-			"Wingardium Leviosa"
-		elsif force == 10
-			"Wingardium Leviosa"
-		else
-			nil
-		end
-	do
-		looping_string = "Wingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\n"
-			expect{ using_until }.to output(looping_string).to_stdout
-	end
-end
+def hogwarts
+  levitation = 6 
+  while levitation < 10
+  puts "Wingardium Leviosa"
+  levitation += 1 
+  end 
+end  
+hogwarts
